745d23450983a55cd4e0ffa3db805b0e54f703b6
Sivasubramanyam A <sivasubramanyama@gmail.com>
2015-08-17 12:51:09 +0530

    place navbar js inside doc.ready to prevent code not running due to minify

--- static/js/main.js
+++ static/js/main.js
@@ -1,41 +1,43 @@
 //Courtest Marius Craciunoiu, medium.com
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = $('nav').outerHeight();
 
-$(window).scroll(function(event){
-    didScroll = true;
-});
-
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
-}, 250);
-
-function hasScrolled() {
-    var st = $(this).scrollTop();
-    
-    // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
-        return;
-    
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight){
-        // Scroll Down
-        $('nav').addClass('nav-up');
-    } else {
-        if(st + $(window).height() < $(document).height()) {
-            $('nav').removeClass('nav-up');
-        }
-    }
-    
-    lastScrollTop = st;
-}
 
 $("document").ready(function() {
     $("img").addClass("img-responsive");
+
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+    var navbarHeight = $('nav').outerHeight();
+
+    $(window).scroll(function(event){
+        didScroll = true;
+    });
+
+    setInterval(function() {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 250);
+
+    function hasScrolled() {
+        var st = $(this).scrollTop();
+        
+        // Make sure they scroll more than delta
+        if(Math.abs(lastScrollTop - st) <= delta)
+            return;
+        
+        // If they scrolled down and are past the navbar, add class .nav-up.
+        // This is necessary so you never see what is "behind" the navbar.
+        if (st > lastScrollTop && st > navbarHeight){
+            // Scroll Down
+            $('nav').addClass('nav-up');
+        } else {
+            if(st + $(window).height() < $(document).height()) {
+                $('nav').removeClass('nav-up');
+            }
+        }
+        
+        lastScrollTop = st;
+    }
 });
