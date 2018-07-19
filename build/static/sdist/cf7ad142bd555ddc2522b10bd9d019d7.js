7d2be144e205bc80d883b29794d24696695c9223
Sivasubramanyam A <ssdragonar5@gmail.com>
2018-07-19 09:46:54 +0530

    Lighthouse changes

---
+++ build/static/sdist/cf7ad142bd555ddc2522b10bd9d019d7.js
@@ -0,0 +1,2 @@
+
+isWebp=()=>document.createElement('canvas').toDataURL('image/webp').startsWith('data:image/webp');if(isWebp()){let html=document.getElementsByTagName('html')[0];html.className='webp';}
