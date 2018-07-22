38b66f8f746bcf12f6486e837b342849a05dd440
Sivasubramanyam A <ssdragonar5@gmail.com>
2018-07-22 22:22:56 +0530

    Move out of Bootstrap

--- build/static/sdist/cf7ad142bd555ddc2522b10bd9d019d7.js
+++
@@ -1,2 +0,0 @@
-
-isWebp=()=>document.createElement('canvas').toDataURL('image/webp').startsWith('data:image/webp');if(isWebp()){let html=document.getElementsByTagName('html')[0];html.className='webp';}
