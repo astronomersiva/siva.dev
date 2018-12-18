690f4934a19ef24915cc8461ba40002f03ab47ca
Sivasubramanyam A <ssdragonar5@gmail.com>
2018-12-19 01:26:30 +0530

    Inline critical CSS

--- lego.js
+++ lego.js
@@ -1,4 +1,19 @@
 module.exports = {
   name: 'www.sivasubramanyam.me',
-  url: 'https://www.sivasubramanyam.me/'
+  url: 'https://www.sivasubramanyam.me/',
+  critical: {
+    inline: true,
+    dimensions: [
+      {
+        height: 800,
+        width: 470
+      }, {
+        height: 900,
+        width: 1200
+      }
+    ],
+    penthouse: {
+      timeout: 150000
+    }
+  }
 }
