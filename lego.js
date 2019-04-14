ff38be5e2b79d2bacaa13b3cdfb53477feb4e124
Sivasubramanyam A <ssdragonar5@gmail.com>
2019-04-14 13:02:27 +0530

    Update to lego v2

--- lego.js
+++ lego.js
@@ -1,12 +1,20 @@
 require('dotenv').config();
 
+const fs = require('fs-extra');
+const path = require('path');
 const md = require('./config/markdown');
 const critical = require('./config/extract-critical');
 const rss = require('./config/rss');
 
-const ssl = process.env.SSL ?
-  { key: 'ssl/server.key', cert: 'ssl/server.crt' } :
-  false;
+const ssl = process.env.SSL
+  ? { key: 'ssl/server.key', cert: 'ssl/server.crt' }
+  : false;
+
+let cacheDir = '.lego';
+if (process.env.NETLIFY_BUILD_BASE) {
+  cacheDir = path.join(process.env.NETLIFY_BUILD_BASE, 'cache', 'lego');
+  fs.mkdirpSync(cacheDir);
+}
 
 module.exports = {
   name: 'siva.dev',
@@ -26,5 +34,6 @@ module.exports = {
   },
   md,
   critical,
-  rss
+  rss,
+  cacheDir
 }
