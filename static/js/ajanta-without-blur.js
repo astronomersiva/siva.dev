d4b1831a2c12aec8f0d554dd2825a882ee902b63
Sivasubramanyam A <ssdragonar5@gmail.com>
2019-09-16 22:50:49 +0530

    Add missing old style lazy loading script

--- static/js/ajanta-without-blur.js
+++ static/js/ajanta-without-blur.js
@@ -1,9 +1,8 @@
-let images = document.getElementsByTagName('img');
-for (let image of images) {
+let imagesInPost = document.getElementsByTagName('img');
+for (let image of imagesInPost) {
   // old handling for lazy loading images
   let lazySrc = image.getAttribute('data-src');
   if (lazySrc) {
     image.src = lazySrc;
   }
-
 }
