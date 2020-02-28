bf8dcc9b07a5f0a57f86500d65f20082551eeb44
Sivasubramanyam A <ssdragonar5@gmail.com>
2020-02-28 22:59:32 +0530

    Private Image CDN

--- static/js/ajanta-without-blur.js
+++ static/js/ajanta-without-blur.js
@@ -2,7 +2,7 @@ let imagesInPost = document.getElementsByTagName('img');
 for (let image of imagesInPost) {
   // old handling for lazy loading images
   let lazySrc = image.getAttribute('data-src');
-  if (lazySrc) {
+  if (lazySrc && image.parentNode && image.parentNode.classList && !image.parentNode.classList.contains('ajanta')) {
     image.src = lazySrc;
   }
 }
--- static/js/ajanta.js
+++ static/js/ajanta.js
@@ -13,11 +13,19 @@ for (let ajanta of ajantaImages) {
 
   let src = pixelated.getAttribute('data-src');
   if (src) {
+    let canResize = !pixelated.hasAttribute('data-skip-cdn');
     let webpSrc = htmlElement.className.includes('webp') && pixelated.getAttribute('webp');
+    let resizeServer = '';
+    if (canResize && !window.location.hostname.includes('127.0.0.1')) {
+      resizeServer = 'https://cdn.sivasubramanyam.me/unsafe';
+      resizeServer = `${resizeServer}/${window.innerWidth < 700 ? '700x' : '1400x'}`;
+      resizeServer = `${resizeServer}/filters:no_upscale()/${window.location.hostname}`;
+    }
+
     if (webpSrc) {
-      original.src = webpSrc;
+      original.src = `${resizeServer}${webpSrc}`;
     } else {
-      original.src = src;
+      original.src = `${resizeServer}${src}`;
     }
   }
 }
