e4b90ecad936f21ac57aae36766e5a81b0859003
Sivasubramanyam A <ssdragonar5@gmail.com>
2020-03-01 13:08:59 +0530

    Load webp if supported

--- static/js/ajanta.js
+++ static/js/ajanta.js
@@ -14,18 +14,14 @@ for (let ajanta of ajantaImages) {
   let src = pixelated.getAttribute('data-src');
   if (src) {
     let canResize = !pixelated.hasAttribute('data-skip-cdn');
-    let webpSrc = htmlElement.className.includes('webp') && pixelated.getAttribute('webp');
+    let isWebpAvailable = htmlElement.className.includes('webp');
     let resizeServer = '';
     if (canResize && !window.location.hostname.includes('127.0.0.1')) {
       resizeServer = 'https://cdn.sivasubramanyam.me/unsafe';
       resizeServer = `${resizeServer}/${window.innerWidth < 700 ? '700x' : '1400x'}`;
-      resizeServer = `${resizeServer}/filters:no_upscale()/${window.location.hostname}`;
+      resizeServer = `${resizeServer}/filters:no_upscale()${isWebpAvailable ? ':format(webp)' : ''}/${window.location.hostname}`;
     }
 
-    if (webpSrc) {
-      original.src = `${resizeServer}${webpSrc}`;
-    } else {
-      original.src = `${resizeServer}${src}`;
-    }
+    original.src = `${resizeServer}${src}`;
   }
 }
