f0f0680b3815856c20ae883807ad232e3889c232
Sivasubramanyam A <ssdragonar5@gmail.com>
2021-08-24 00:10:41 +0530

    Add notes

---
+++ data/notes.js
@@ -0,0 +1,29 @@
+const notes = [
+  {
+    title: '`async` functions may not work while using `page.evaluate` in Puppeteer.',
+    link: 'https://github.com/puppeteer/puppeteer/issues/1665#issuecomment-354241717',
+    content: 'This was a surprising bug. \
+      Turns out, Puppeteer serializes these functions using `Function.prototype.toString()`. \
+      Babel, and other transpilers, change the code in such a way that this no longer works. \
+      The solution is to pass the function as a template literal or configure the transpiler \
+      to use the latest ecma version(`"target": "es2018"`). \
+    ',
+    id: 'puppeteer-async'
+  }, {
+    title: 'Choosing the right gear for planetary astrophotography',
+    content: 'Try and hit a focal ratio of `5 x pixel size`. You may need a Barlow to hit this number.',
+    link: 'https://www.cloudynights.com/topic/700833-why-does-the-f-num-5x-pixel-size-apply-for-colour-cameras/',
+    id: 'planetary-cam'
+  }
+];
+
+const regex = /`([a-zA-Z0-9\.\-\_\(\)\:\'\"\s]+)`/g;
+const formattedNotes = notes.map(note => {
+  return {
+    ...note,
+    title: note.title && note.title.replace(regex, '<code>$1</code>'),
+    content: note.content.replace(regex, '<code>$1</code>'),
+  };
+});
+
+module.exports = formattedNotes;
